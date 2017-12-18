765d5e1b78a1254ae37c9a7981db8f96c87b877a
zhouhq <158103597@qq.com>
2017-12-18 16:05:59 +0800

    1.把通用的view独立到Commong工程中 2.增加了一个

---
+++ app/src/main/java/com/ryo/happymoment/recommend/RecommendProvider.java
@@ -0,0 +1,100 @@
+package com.ryo.happymoment.recommend;
+
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import com.ryo.data.RyoDataBaseHelper;
+
+/**
+ * Created by Administrator on 2017/12/18.
+ */
+
+public class RecommendProvider extends ContentProvider {
+    private static UriMatcher uriMatcher;
+    private static final int RECOMMEND_DIR = 0;
+    private static final int RECOMMEND_ITEM = 1;
+    private static final String RECOMMEND_TABLE = "recommend_table";
+    public static final String AUTHORITY = "com.ryo.happymoment.provider";
+    public static final String Recommend_Uri="content://" + AUTHORITY + "/" + RECOMMEND_TABLE;
+    private RyoDataBaseHelper dataBaseHelper;
+
+    static {
+
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, "recommend", RECOMMEND_DIR);
+        uriMatcher.addURI(AUTHORITY, "recommend/#", RECOMMEND_ITEM);
+
+    }
+
+    @Override
+    public boolean onCreate() {
+        try {
+            dataBaseHelper = new RyoDataBaseHelper(this.getContext(), "recommend.db", null, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] column, String selection, String[] selectionArgs, String sortOrder) {
+        Cursor cursor = null;
+        SQLiteDatabase database = dataBaseHelper.getWritableDatabase();
+        switch (uriMatcher.match(uri)) {
+            case RECOMMEND_DIR:
+                cursor = database.query(RECOMMEND_TABLE, column, selection, selectionArgs, null, null, sortOrder);
+                break;
+
+            case RECOMMEND_ITEM:
+                String recommendID = uri.getPathSegments().get(1);
+                cursor = database.query(RECOMMEND_TABLE, column, "id= ?", new String[]{recommendID}, null, null, sortOrder);
+                break;
+        }
+        return cursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            case RECOMMEND_DIR:
+                return "vnd.android.cursor.dir/vnd.com.ryo.com.ryo.happymoment.provider" + RECOMMEND_TABLE;
+            case RECOMMEND_ITEM:
+                return "vnd.android.cursor.item/vnd.com.ryo.com.ryo.happymoment.provider" + RECOMMEND_TABLE;
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        Uri resultUri = null;
+        SQLiteDatabase database = dataBaseHelper.getWritableDatabase();
+        switch (uriMatcher.match(uri)) {
+            case RECOMMEND_DIR:
+            case RECOMMEND_ITEM:
+                long recommend = database.insert(RECOMMEND_TABLE, null, contentValues);
+                resultUri = Uri.parse("content://" + AUTHORITY + "/" + RECOMMEND_TABLE + recommend);
+                break;
+            default:
+                break;
+        }
+        return resultUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
